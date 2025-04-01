7a7e3ba301945a6689f4f0dd46170423ec3ac146
Dinoking1137 <athekosoff@gmail.com>
2025-04-01 15:07:42 -0700

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,21 @@
+# Aaron Kosoff
+# 4/1/2025
+# ASTR 19
+######################################################
+#
+# This program will write "Hello World!"
+
+# This function tells the computer to call a print statement holding the string "Hello World!"
+def PrintHelloWorld():
+	
+	print("Hello World!");
+
+# This defines the main() function for our program
+def main():
+
+	PrintHelloWorld()
+
+# When we run the program, this executes first
+if __name__=='__main__'
+
+	main()
